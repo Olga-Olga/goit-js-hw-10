9c20efeb603ed778e694af9f8590d045e9bb6d28
Olga-Olga <olga_l@ukr.net>
2023-07-08 20:37:16 +0300

    1234

--- src/fetch.js
+++ src/fetch.js
@@ -2,6 +2,7 @@ import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_A0QBC77czTXXzSvyN9Zimoxe9AmubwRr0tlg7oEnDYZrJyNnbnxe9tUN4nNfQjnN";
 // console.log(axios.isCancel('something'));
 
+
 export default class FetchCat {
         constructor(url) {
         this.url = url;
@@ -11,18 +12,29 @@ export default class FetchCat {
             return axios.get(url, {
             })
                     .then(res => {
-                            console.log(typeof(res.data));
                             return res.data
                     })
                 // .then(data => {
                 //     data.forEach(el => {
-                //         // arrCats.push(`{${el.id}, ${el.name}}`)
                 //         arrCats.push({ id: el.id, name: el.name })
-                        
+                //     })
+                // })
+        // return arrCats
+}
+
+        fetchCatByBreed(brirdId) {
+            // const arrCats = []
+            return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${brirdId}`, {
+            })
+                    .then(res => {
+                            return res.data
+                    })
+                // .then(data => {
+                //     data.forEach(el => {
+                //         arrCats.push({ id: el.id, name: el.name })
                 //     })
                 // })
         // return arrCats
         }
-
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 
 import FetchCat from "./fetch";
 
-//  import markup from './templates/markupCat.hbs'
+// const Handlebars = require("handlebars");
+// const template = Handlebars.compile(`{{#each this}}
+// <option value={{${el.id}}}>{{${el.name}}}</option>
+// {{/each}}`);
+// console.log(template());
+
+// import markup from './templates/markupCat.hbs';
+
 
 const dropDownEl = document.querySelector(".breed-select")
 
@@ -14,19 +21,47 @@ catArr.fetchCats(allCatsURL)
     .then(data => {
        
         const mark = data.map(el => {
-        //    console.log(el);
     return `<option value=${el.id}>${el.name}</option>`
         }).join("")
-dropDownEl.insertAdjacentHTML("beforeEnd", mark)
-        
+        dropDownEl.insertAdjacentHTML("beforeEnd", mark)
+    
     })
 
 
 dropDownEl.addEventListener("change", handleDataCat)
-
-// console.log(dropDownEl);
 function handleDataCat(event) {
     console.dir(event.target.value)
+    catArr.fetchCatByBreed(event.target.value).then(el => {        
+        const id = event.target.value
+        const nameCat = el[0].breeds[0].name
+        const picture = el[0].url
+        const description = el[0].breeds[0].description
+        const wiki = el[0].breeds[0].wikipedia_url
+        const temperament = el[0].breeds[0].temperament
 
+        const mimimi = 
+ `<img src="${picture}" alt="${id}" />
+      <div>
+        <h2>${nameCat}</h2>
+        <p>${description}</p>
+        <h1>Temperament: ${temperament}</h1>
+        <h1>${wiki}</h1>
+</div>`  
+        console.log(mimimi);
+
+const mark1 = el.map(el => {
+    return mimimi
+}).join("")
+        
+        const card = document.querySelector(".cat-info")
+
+        console.log(card);
+        card.insertAdjacentHTML("beforeEnd", mark1)
+
+        })
+
+    
 }
+
+
 
