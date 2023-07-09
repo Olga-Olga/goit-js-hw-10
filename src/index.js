903934ebfad8e9a7a06d1e746410362e1e66ddfa
Olga-Olga <olga_l@ukr.net>
2023-07-09 12:36:45 +0300

    123

--- src/index.js
+++ src/index.js
@@ -1,40 +1,61 @@
 
-import {FetchCat} from "./cat-api";
-
+import { FetchCat } from "./cat-api";
+// import SlimSelect from 'slim-select'
+// import NiceSelect from "nice-select2";
+// import VirtualSelect from 'virtual-select-plugin';
 // const Handlebars = require("handlebars");
 // const template = Handlebars.compile(`{{#each this}}
 // <option value={{${el.id}}}>{{${el.name}}}</option>
 // {{/each}}`);
 // console.log(template());
 // import markup from './templates/markupCat.hbs';
+import Notiflix from 'notiflix';
+const liba = ""
 const dropDownEl = document.querySelector(".breed-select")
+
 const loaderEl = document.querySelector(".loader")
 const errorEl = document.querySelector(".error")
 const cardCatEl = document.querySelector(".cat-info");
-
 
 const ALL_CATS_URL = "https://api.thecatapi.com/v1/breeds"
 const catArr = new FetchCat(ALL_CATS_URL)
                
 catArr.fetchCats(ALL_CATS_URL)
         .then(data => {
-       cardCatEl.classList.add("is-hidden");
-        const mark = data.map(el => {
-    return `<option value=${el.id}>${el.name}</option>`
-        }).join("")
-        dropDownEl.insertAdjacentHTML("beforeEnd", mark)
-    
-    }).catch(console.log)
+            cardCatEl.classList.add("is-hidden");
+            const mark = data.map(el => {
+                return `<option value=${el.id}>${el.name}</option>`
+                // console.log(`{label: "${el.name}", value="${el.id}"}`);
+                return `{label: "${el.name}", value="${el.id}"}`;
+            }).join("")
+            // VirtualSelect.init({ ele: 'select', options: [mark], placeholder: 'Select options here'});
+            dropDownEl.insertAdjacentHTML("beforeEnd", mark)
+            VirtualSelect.init({ ele: dropDownEl });        
+          
+            const el = document.querySelector('.breed-select')
+            el.addEventListener("change", handleDataCat)
+     }).catch(el => Notiflix.Notify.info('Cogito ergo sum'))
 
 
+// new VirtualSelect(dropDownEl);
+// var instancce = NiceSelect.bind(document.querySelector(".breed-select")).update()
 const ONE_CAT_BASE_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=`
 
-dropDownEl.addEventListener("change", handleDataCat)
+dropDownEl.addEventListener('change', function() {
+  console.log("this value:", this.value);
+});
+
+
+document.querySelector('.breed-select').addEventListener("change", handleDataCat)
 function handleDataCat(event) {
+    console.log(this.value);
+    console.log(123);
     console.dir(event.target.value)
+    console.dir(event.currentTarget)
+
     catArr.fetchCatByBreed(ONE_CAT_BASE_URL + event.target.value).then(el => {
         cardCatEl.classList.remove("is-hidden");
-        
+
         const id = event.target.value
         const nameCat = el[0].breeds[0].name
         const picture = el[0].url
@@ -56,7 +77,7 @@ function handleDataCat(event) {
         cardCatEl.innerHTML = kittyCatMarkup
         // cardCatEl.insertAdjacentHTML("beforeEnd", mark1)
     })
-        .catch(console.log)
+        .catch(el => Notiflix.Notify.info('Cogito ergo sum'))
 }
 
 
